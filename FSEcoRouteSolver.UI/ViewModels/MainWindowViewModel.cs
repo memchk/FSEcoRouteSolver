20df6b189d643a0d245843fee2b3de69c77232fc
Carson Page <carson.page@byterealm.com>
2019-12-25 17:35:44 -0600

    First pass at avalonia

---
+++ FSEcoRouteSolver.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FSEcoRouteSolver.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentPage => new SolverConfigurationViewModel();
+    }
+}
---
+++ FSEcoRouteSolver.UI/ViewModels/SolverConfigurationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FSEcoRouteSolver.UI.ViewModels
+{
+    class SolverConfigurationViewModel : ViewModelBase
+    {
+    }
+}
